a771c9a0f75b11c3c0b70f9fed1146b89d5ee31d
iamhosto10 <gerardoramirez4400@gmail.com>
2023-03-31 09:36:11 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// sdk metamap
+import { useEffect } from "react";
+import { Button, NativeEventEmitter, NativeModules, View } from "react-native";
+import { MetaMapRNSdk } from "react-native-expo-metamap-sdk";
 
 export default function App() {
+  useEffect(() => {
+    const MetaMapVerifyResult = new NativeEventEmitter(
+      NativeModules.MetaMapRNSdk
+    );
+    MetaMapVerifyResult.addListener("verificationSuccess", (data) =>
+      console.log(data)
+    );
+    MetaMapVerifyResult.addListener("verificationCanceled", (data) =>
+      console.log(data)
+    );
+  });
+  const handleMetaMapClickButton = () => {
+    console.log("entro", MetaMapRNSdk);
+    //set 3 params clientId (cant be null), flowId, metadata
+    var yourMetadata = { param1: "value1", param2: "value2" };
+    MetaMapRNSdk.showFlow(
+      "610b96fb7cc893001b135505",
+      "611101668083a1001b13cc80",
+      yourMetadata
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "powderblue",
+      }}
+    >
+      <Button onPress={() => handleMetaMapClickButton()} title="Click here" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
